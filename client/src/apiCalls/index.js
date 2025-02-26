721a5f82709a902c66f558e172ee7e60d846c639
Damilola Adebowale <adebowaletemitayo40@gmail.com>
2025-02-26 11:27:03 +0100

    update API endpoint to production URL and adjust asset references

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:5000";
+export const url = "https://chatapp-9rl2.onrender.com";
 
 export const axiosInstance = axios.create({
   headers: {
